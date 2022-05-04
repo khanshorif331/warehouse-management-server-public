4d05b3c9fd44b7f0f00090243fc32b1d81a3f1f2
khanshorif331 <khanshorif331@gmail.com>
2022-05-04 07:05:04 +0600

    created route to get the items

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env.PORT || 5000
 
-// Replace the uri string with your MongoDB deployment's connection string.
-// const uri =
-// 	'mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority'
-
-// const client = new MongoClient(uri)
-
-// khanshorif
-// FSGi0uKIHeqJUQBA
+// middleware
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ntwoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
@@ -24,15 +20,15 @@ async function run() {
 	try {
 		await client.connect()
 		console.log('Database Connencted')
-
 		const productCollection = client.db('warehouse').collection('bike')
-		// const movies = collection('movies');
+		console.log(productCollection)
 
-		// // Query for a movie that has the title 'Back to the Future'
-		// const query = { title: 'Back to the Future' };
-		// const movie = await movies.findOne(query);
-
-		// console.log(movie);
+		app.get('/items', async (req, res) => {
+			const query = {}
+			const cursor = productCollection.find()
+			const products = await cursor.toArray()
+			res.send(products)
+		})
 	} finally {
 	}
 }
